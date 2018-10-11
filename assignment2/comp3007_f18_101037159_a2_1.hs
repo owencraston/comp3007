aad730e97cd527d555426fbc9b53d9961924b059
Owen Craston <owen.craston@shopify.com>
2018-10-11 11:37:27 -0400

    question 1a

---
+++ assignment2/comp3007_f18_101037159_a2_1.hs
@@ -0,0 +1,41 @@
+import Codec.BMP
+import GHC.Word
+import Data.ByteString
+
+-- For this code to work you will need to have installed the package "bmp-1.2.6.3"
+-- You can do this using the cabal package manager (as demonstrated in class)
+-- or you can follow a procedure similar to the one detailed below:
+--
+-- 1. Download bmp-1.2.6.3.tar.gz from http://hackage.haskell.org/package/bmp
+-- 2. Extract the folder bmp-1.2.6.3
+-- 3. Open a terminal (with administrator rights) and do the following:
+--    a. cd into bmp-1.2.6.3
+--    b. runhaskell Setup configure
+--    c. runhaskell Setup build
+--    d. runhaskell Setup install
+
+loadBitmapIntoIt :: FilePath -> IO ([(Int, Int, Int)], Int, Int)
+loadBitmapIntoIt filename = do
+  (Right bmp) <- readBMP filename
+  return ((parseIntoRGBVals (convertToIntList (unpack (unpackBMPToRGBA32 bmp)))), (fst (bmpDimensions bmp)), (snd (bmpDimensions bmp)))
+
+convertToIntList :: [Word8] -> [Int]
+convertToIntList [] = []
+convertToIntList (h:t) = (fromIntegral (toInteger h)) : (convertToIntList t)
+
+parseIntoRGBVals :: [Int] -> [(Int, Int, Int)]
+parseIntoRGBVals [] = []
+parseIntoRGBVals (h:i:j:_:t) = (h,i,j) : (parseIntoRGBVals t)
+
+-- if you haven't written showAsASCIIArt' yet, then comment out the function below
+
+-- showAsASCIIArt :: [[(Int, Int, Int)]] -> IO ()
+-- showAsASCIIArt pixels = Prelude.putStr (unlines (showAsASCIIArt' pixels) )
+
+
+-- convertToList :: ([(Int, Int, Int)], Int, Int) -> [[(Int, Int, Int)]]
+-- convertToList is the function I have written to address requirement (a)
+
+convertToList :: ([(Int, Int, Int)], Int, Int) -> [[(Int, Int, Int)]]
+convertToList (_, _, 0) = []
+convertToList (l, w, h) = take w l : convertToList (drop w l , w, h - 1)
