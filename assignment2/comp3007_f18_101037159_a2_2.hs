f4ea8dfb4ddcf223b0280c155b322381bf57ee9a
Owen Craston <owen.craston@shopify.com>
2018-10-12 14:52:20 -0400

    ternary operands

--- assignment2/comp3007_f18_101037159_a2_2.hs
+++ assignment2/comp3007_f18_101037159_a2_2.hs
@@ -1,1 +1,19 @@
 data TVL = falz | troo | unknown
+
+-- ternaryNot flips the value unles it is unknown
+ternaryNOT :: TVL -> TVL
+ternaryNOT falz = troo
+ternaryNOT troo = falz
+ternaryNOT unknown = unknown
+
+ternaryAND :: TVL TVL - TVL
+ternaryAnd falz _ = falz
+ternaryAnd _ falz = falz
+ternaryAnd troo troo = troo
+ternaryAnd _ _ = unknown
+
+ternaryOR :: TVL TVL - TVL
+ternaryOR troo _ = troo
+ternaryOR troo _ = troo
+ternaryOR falz falz = troo
+ternaryOR _ _ = unknown
