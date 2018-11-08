cbb761414faba840a17339225f61f359c7e46c94
Owen Craston <owen.craston@shopify.com>
2018-11-07 23:05:44 -0500

    question1 v1

--- assignment3/comp3007_f18_101037159_a3.hs
+++ assignment3/comp3007_f18_101037159_a3.hs
@@ -3,7 +3,21 @@ import GHC.Word
 import GHC.List as List
 import Data.ByteString
 
+
+-- Owen Craston
+-- 101037159
+-- comp 3007 assignment 3
+
 -- question1
+randf :: Int -> Float
+randf f = fromIntegral (rand f :: Int) / 10
+
+rand :: Int -> Int
+rand i = randNum
+  where randNum = (x - 1) `mod` 10
+        x = 3 * (i) `mod` 101
+
+
 -- question 2
 -- question 3
 -- question 4
